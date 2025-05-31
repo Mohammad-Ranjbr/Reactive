372832962681dfd929cd11dcae1d9cecd9b173ff
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2025-05-31 10:43:30 +0330

    On Error Dropped - Problem

--- src/main/java/com/example/reactorprogrammingplayground/sec02/Lec04MonoEmptyError.java
+++ src/main/java/com/example/reactorprogrammingplayground/sec02/Lec04MonoEmptyError.java
@@ -12,6 +12,20 @@ public class Lec04MonoEmptyError {
         // Because an error occurred, only onError() is executed. Neither onNext() nor onComplete() are executed.
         getUsername(3).subscribe(Util.subscriber("sub3"));
 
+        //Understanding an error message like:
+        //Operator called default onErrorDropped
+        //Suppose we have the following code:
+        //Mono<String> mono = getUserName(3); // Wrong value → Mono.error(...)
+        //mono.subscribe(name -> System.out.println(name));
+        //In this code:
+        //Only onNext is defined (Consumer<String>)
+        //But since userId is 3, the getUserName method returns an error (Mono.error(...)).
+        //Since you haven't specified any handler for the error (onError), when Mono.error(...) is executed,
+        //Reactor doesn't know how to handle that error. So: it drops it. And a warning message is printed like above
+        //Solution: Just add the error handler when subscribing
+        getUsername(3)
+                .subscribe(s -> System.out.println(s));
+
     }
 
     //In reactive programming:
