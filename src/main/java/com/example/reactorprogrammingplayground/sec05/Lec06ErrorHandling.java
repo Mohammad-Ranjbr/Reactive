c368246401a3f6accf837a7d16502d1db0b97626
Mohammad-Ranjbr <mohammadranjbar.mmr81@gmail.com>
2025-06-10 11:33:25 +0330

    Error Handling - Part1 , Error Handling - Part 2

---
+++ src/main/java/com/example/reactorprogrammingplayground/sec05/Lec06ErrorHandling.java
@@ -0,0 +1,59 @@
+package com.example.reactorprogrammingplayground.sec05;
+
+import com.example.reactorprogrammingplayground.common.Util;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import reactor.core.publisher.Mono;
+
+public class Lec06ErrorHandling {
+    private static final Logger log = LoggerFactory.getLogger(Lec06ErrorHandling.class);
+    public static void main(String[] args) {
+
+        demo2();
+
+    }
+
+    // when you want to return a hardcoded value / simple computation
+    private static void onErrorReturn(){
+        //If an error occurs during the execution of Flux or Mono (for example, division by zero or failure in a network request),
+        //the flow is immediately stopped and onError is executed. Solution: Use onErrorReturn
+        //If you put onErrorReturn before the map operator, the error will not be handled in the map
+        //You can define multiple onErrorReturns for different types of exceptions. The first one that matches the exception type is executed.
+        //onErrorReturn(value) returns value on any error
+        //onErrorReturn(type, value) returns value only on the exception to the specified type
+        Mono.just(5)
+                .map(i -> i == 5 ? 5/0 : i)
+                //.onErrorReturn(-1)
+                .onErrorReturn(IllegalArgumentException.class, -1)
+                .onErrorReturn(ArithmeticException.class, -2)
+                .onErrorReturn(-3)
+                .subscribe(Util.subscriber());
+    }
+
+    //Sometimes we don't want to return a constant value, but rather want to use another service in case of an error.
+    //Solution: Use onErrorResume to return a new Mono or Flux in case of an error.
+    private static Mono<Integer> fallback1(){
+        return Mono.fromSupplier(() -> Util.faker().random().nextInt(10, 100));
+    }
+
+    private static Mono<Integer> fallback2(){
+        return Mono.fromSupplier(() -> Util.faker().random().nextInt(100, 1000));
+    }
+
+    private static void demo1(){
+        Mono.error(new ArithmeticException("oops"))
+                .onErrorResume(ArithmeticException.class, ex -> fallback1())
+                .onErrorResume(ex -> fallback2())
+                .onErrorReturn(-5)
+                .subscribe(Util.subscriber());
+    }
+
+    private static void demo2(){
+        Mono.error(new RuntimeException("oops"))
+                .onErrorResume(ArithmeticException.class, ex -> fallback1())
+                .onErrorResume(ex -> fallback2())
+                .onErrorReturn(-5)
+                .subscribe(Util.subscriber());
+    }
+
+}
